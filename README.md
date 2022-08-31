90a2b3508d297c307c166f687717bf0190779786
underson666666 <underson666666@gmail.com>
2022-08-31 20:33:05 +0900

    I implemented the minimum functionality and write README.

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # ping-monitor-line-notify
+
+This is command line tool that is to nofity 'line notify' with ping monitoring.
+
+## How to use
+
+### Environment valiables
+
+You need to set envinronment varialbe 'LINE_API_TOKEN'.
+```sh
+export LINE_API_TOKEN='<you token>`
+`'```
+
+### Monitoring target
+
+Please change the value of valiable `monitor_target` in the python file.  
+Maybe soon I will change to accept `monitor_target` from command line arguments. Maybe.
+
+### Install packages
+You need to install python packages like this.
+```sh
+pip install -r requirements.txt
+```
+
+### Execute
+
+just like this.
+```sh
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+
+import requests
+from ping3 import ping
+
+LINE_NOTIFY_API = "https://notify-api.line.me/api/notify"
+
+
+def main():
+    monitor_target = "192.168.1.4"
+    is_connected = ping_monitoring(monitor_target)
+    if not is_connected:
+        msg = f"NOT Connect to {monitor_target}"
+        notify_line(msg)
+
+
+def ping_monitoring(target: str) -> bool:
+    """指定したIPアドレスにpingを行い、疎通ができたかを返す
+
+    Parameters
+    ----------
+    target: str
+        IPv4 address
+
+    Returns
+    -------
+    bool: whether had connected or not
+    """
+    result = ping(target)
+    return isinstance(result, float)
+
+
+def notify_line(message: str):
+    """notify to line notify
+
+    Parameters
+    ----------
+    message: str
+        通知するメッセージ
+    """
+    api_token = os.getenv("LINE_API_TOKEN")
+    headers = {"Authorization": f"Bearer {api_token}"}
+    data = {"message": message}
+    res = requests.post(LINE_NOTIFY_API, headers=headers, data=data)
+
+    if res.status_code != requests.codes.ok:
+        print("failed to send message.")
+
+
+if __name__ == "__main__":
+    main()
